e7d5c2c5e85719ff1b33c479ed519bb06b82777f
star-lord-97 <a_mahmoud_1997@yahoo.com>
2019-09-22 05:59:02 +0200

    2nd day, 13 videos

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
-class SignIn extends Component {
+class SignUp extends Component {
   state = {
+    firstName: '',
+    lastName: '',
     email: '',
     password: ''
   };
@@ -14,14 +16,22 @@ class SignIn extends Component {
   handleChange = (e) => {
     this.setState({
       [e.target.id]: e.target.value
-    })
+    });
   };
 
   render() {
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Login</h5>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
+          </div>
           <div className="input-field">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" onChange={this.handleChange} />
@@ -31,7 +41,7 @@ class SignIn extends Component {
             <input type="password" id="password" onChange={this.handleChange} />
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
           </div>
         </form>
       </div>
@@ -39,4 +49,4 @@ class SignIn extends Component {
   };
 };
 
-export default SignIn;
+export default SignUp;
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,29 +1,14 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
-const ProjectList = () => {
+const ProjectList = ({projects}) => {
   return(
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by the Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
+      {projects && projects.map(project => {
+        return(
+          <ProjectSummary project={project} key={project.id} />
+        )
+      })}
     </div>
   );
 };
